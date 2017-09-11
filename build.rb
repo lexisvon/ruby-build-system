fa6d8c01cb801e346d379ebac3bbaf7be5d154dd
Lexis von Seggern <lexvon2015@gmail.com>
2017-09-11 10:20:11 -0500

    ready to start

---
+++ build.rb
@@ -0,0 +1,8 @@
+# Given a specific list of files:
+# - buttons.css
+# - layout.css
+# - type.css
+
+# Get the String contents of each file. Store them all, one by one, in a new String.
+
+# Then output that String into a single styles.css file. 
